09c563ccc941ace79c5855b55de0e2d63f01ca02
Massi1030 <issam-012@hotmail.com>
2019-06-25 13:49:55 +0200

    déplacement du robot.py

--- basic_remote/remote.py
+++ basic_remote/remote.py
@@ -11,30 +11,44 @@ import time
 
 print("z : avance\nq : gauche\ns : reculer\nd : droite\ne/r : led\na : stop\nc : exit")
 print("entre une lettre pour piloter le robot comme dans la description")
-lettre = input(">>")
-if lettre == z:
-    spytank.avance(125)
-elif lettre == q:
-    spytank.gauche(125)
-elif lettre == s:
-    spytank.recule(-125)
-elif lettre == d:
-    spytank.droite(125)
-elif lettre == a:
-    spytank.stop
 
-elif lettre == e:
+continuer = True
+while continuer
+
+lettre = input(">>")
+
+
+if lettre == "z":
+    spytank.avance(125)
+elif lettre == "q":
+    spytank.gauche(125)
+elif lettre == "s":
+    spytank.recule(-125)
+elif lettre == "d":
+    spytank.droite(125)
+elif lettre == "a":
+    spytank.stop()
+
+elif lettre == "e":
     spytank.led(0,1)
     spytank.led(1,1)
     spytank.led(2,1)
     spytank.led(3,1)
 
-
-
-elif lettre == r:
+elif lettre == "r":
     spytank.led(0,0)
     spytank.led(1,0)
     spytank.led(2,0)
     spytank.led(3,0)
-elif lettre == c:
-    spytank.exit
+
+elif lettre == "c":
+    spytank.exit()
+
+elif lettre == "a" :
+    spytank.stop()
+
+elif lettre == "c" :
+    spytank.stop()
+    continuer =False
+
+
